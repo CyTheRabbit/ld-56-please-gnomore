ca0302f7fd9f65ceff5f2b1123d5f06a9e7e28e6
Cyrill Podrez <cytherabbit@gmail.com>
2024-10-05 23:29:56 +0300

    Refactored crowd control

--- Assets/Code/LeaderController.cs
+++ Assets/Code/LeaderController.cs
@@ -1,62 +1,104 @@
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using Unity.Collections;
 using UnityEngine;
 
 namespace Gnome
 {
     public class LeaderController : MonoBehaviour
     {
+        public struct NavigationData
+        {
+            public int Index;
+            public int Rank;
+            public float RankDistance;
+            public Vector2 CurrentPosition;
+            public GnomeMovement.Target Target;
+        }
+
+        private struct NavigationRankComparer : IComparer<NavigationData>
+        {
+            public int Compare(NavigationData x, NavigationData y) =>
+                x.RankDistance.CompareTo(y.RankDistance);
+        }
+        
         public GnomeMovement Leader;
         public List<GnomeMovement> Followers;
         [Range(0, 1)]
         public float Overtaking = 0.25f;
         public float AvoidanceDistance = 3;
 
-        private IEnumerable<GnomeMovement> followersByDistance;
-
-        private void Awake()
-        {
-            followersByDistance =
-                Followers.OrderBy(follower => Vector2.Distance(Leader.Position, follower.Position));
-        }
-
         public void FixedUpdate()
         {
-            if (Leader.Destination is not { } destination)
+            if (Leader.Destination is not { } destination) return;
+
+            using var nativeNavigations = new NativeArray<NavigationData>(Followers.Count, Allocator.Temp);
+            var navigations = nativeNavigations.AsSpan();
+            var leaderPosition = Leader.Position;
+
+            InitNavigationData(navigations, leaderPosition);
+            AssignRanks(nativeNavigations, navigations);
+
+            var leaderDirection = (destination.Position - leaderPosition).normalized;
+            var leaderTarget = Vector2.Lerp(leaderPosition, destination.Position, Overtaking);
+            for (var i = 0; i < navigations.Length; i++)
             {
-                return;
+                ref var follower = ref navigations[i];
+
+                Debug.DrawLine(follower.CurrentPosition, leaderTarget, Color.blue);
+
+                follower.Target = new GnomeMovement.Target
+                {
+                    Position = leaderTarget,
+                    Radius = 0.5f + follower.Rank * 0.1f,
+                };
             }
 
-            var toLeader = Vector2.Lerp(Leader.Position, destination.Position, Overtaking);
-            var leaderRay = new Ray2D(Leader.Position, destination.Position - Leader.Position);
-            var i = 1;
-            foreach (var follower in followersByDistance)
+            for (var i = 0; i < navigations.Length; i++)
             {
-                i++;
-                var radius = i * 0.15f;
+                ref var follower = ref navigations[i];
+                var toFollower = follower.CurrentPosition - leaderPosition;
+                var distanceInverse = Mathf.Clamp01(0.3f / toFollower.magnitude);
+                var avoidanceFactor = Mathf.Clamp01(Vector2.Dot(toFollower.normalized, leaderDirection) - distanceInverse);
+                var avoidSide = Cross(leaderDirection, toFollower) > 0 ? 1 : -1;
+                var avoid = leaderPosition + avoidSide * AvoidanceDistance * Vector2.Perpendicular(leaderDirection);
 
-                var toFollower = follower.Position - Leader.Position;
-                var avoidanceFactor = Mathf.Clamp01(Vector2.Dot(toFollower.normalized, leaderRay.direction) - 0.3f);
-                var avoidSide = Cross(leaderRay.direction, toFollower) > 0 ? 1 : -1;
-                var avoid = Leader.Position + Vector2.Perpendicular(leaderRay.direction) * avoidSide * AvoidanceDistance;
-                var target = Vector2.Lerp(toLeader, avoid, avoidanceFactor);
+                Debug.DrawLine(follower.CurrentPosition, avoid, Color.green);
 
-                Debug.DrawLine(follower.Position, toLeader, Color.blue);
-                Debug.DrawLine(follower.Position, avoid, Color.green);
-                Debug.DrawLine(follower.Position, target, Color.yellow);
+                follower.Target = new GnomeMovement.Target
+                {
+                    Position = Vector2.Lerp(follower.Target.Position, avoid, avoidanceFactor),
+                    Radius = Mathf.Lerp(follower.Target.Radius, 0.1f, avoidanceFactor),
+                };
+            }
 
-                follower.Destination = new GnomeMovement.Target
-                    {
-                        Position = target,
-                        Radius = radius,
-                    };
-
+            foreach (var navigation in navigations)
+            {
+                Debug.DrawLine(navigation.CurrentPosition, navigation.Target.Position, Color.yellow);
+                Followers[navigation.Index].Destination = navigation.Target;
             }
         }
 
-        private float Cross(Vector3 a, Vector3 b)
+        private void InitNavigationData(Span<NavigationData> navigations, Vector2 leaderPosition)
         {
-            return a.x * b.y - a.y * b.x;
+            for (var i = 0; i < navigations.Length; i++)
+            {
+                ref var navigation = ref navigations[i];
+                navigation.Index = i;
+                navigation.CurrentPosition = Followers[i].Position;
+                navigation.RankDistance = Vector2.Distance(leaderPosition, navigation.CurrentPosition);
+            }
         }
+
+        private static void AssignRanks(NativeArray<NavigationData> nativeNavigations, Span<NavigationData> navigations)
+        {
+            nativeNavigations.Sort(new NavigationRankComparer());
+            for (var i = 0; i < nativeNavigations.Length; i++)
+            {
+                navigations[i].Rank = i;
+            }
+        }
+
+        private static float Cross(Vector3 a, Vector3 b) => a.x * b.y - a.y * b.x;
     }
 }
