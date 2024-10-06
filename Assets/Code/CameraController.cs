77a0198dd789668e6c3f213e2b4b4a3d3918b0f3
Cyrill Podrez <cytherabbit@gmail.com>
2024-10-06 11:36:18 +0300

    Added editor rooms grid

--- Assets/Code/CameraController.cs
+++ Assets/Code/CameraController.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace Gnome
@@ -35,6 +36,34 @@ namespace Gnome
             var screenTile = Vector2Int.RoundToInt(transform.position / screenSize); 
             var target = Vector2.Lerp(transform.position, screenTile * screenSize, AreaStrength);
             CameraTransform.position = Vector2.SmoothDamp(CameraTransform.position, target, ref velocity, SmoothTime);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.black;
+
+            // Get the scene view camera
+            var sceneCamera = Camera.current;
+            if (sceneCamera == null)
+                return;
+
+            var screenSize = new Vector2(16, 9);
+
+            var cameraPos = sceneCamera.transform.position;
+
+            var minX = Mathf.FloorToInt((cameraPos.x - sceneCamera.orthographicSize * sceneCamera.aspect) / screenSize.x - 1);
+            var maxX = Mathf.CeilToInt((cameraPos.x + sceneCamera.orthographicSize * sceneCamera.aspect) / screenSize.x + 1);
+
+            var minY = Mathf.FloorToInt((cameraPos.y - sceneCamera.orthographicSize) / screenSize.y - 1);
+            var maxY = Mathf.CeilToInt((cameraPos.y + sceneCamera.orthographicSize) / screenSize.y + 1);
+
+            var rooms = new RectInt(minX, minY, maxX - minX, maxY - minY);
+            var halfIndexOffset = new Vector2(0.5f, 0.5f);
+            foreach (var roomIndex in rooms.allPositionsWithin)
+            {
+                var roomRect = new Rect((roomIndex - halfIndexOffset) * screenSize, screenSize);
+                Gizmos.DrawWireCube(roomRect.center, roomRect.size);
+            }
         }
     }
 }
